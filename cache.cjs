a6777c551dbbee3c3ffd652b1452b8ef9b044d3f
Near.Watch <89860717+nearwatch@users.noreply.github.com>
2024-02-26 16:34:41 +0500

    Update cache.cjs

--- cache.cjs
+++ cache.cjs
@@ -1,22 +1,27 @@
 const crypto = require('crypto')
-const cache  = {}
+const cache  = {}, count = {}
 
 setInterval(()=>{
+	let size = 0
 	const d = Date.now()
 	for (const key of Object.keys(cache))
-		if (!cache[key]?.date || d-cache[key].date > 180000) delete cache[key]
-},180000)
+		if (!cache[key]?.date || d-cache[key].date > 3600000){
+			delete cache[key]
+			delete count[key]
+		} else size += cache[key].file.length
+	console.log('cache size:', size)
+},900000)
 
-exports.saveFile = (png) => {
-	const key = crypto.randomBytes(16).toString('hex')
-	cache[key] = {date:Date.now(), file:png}
-	return key
-}
 exports.sendFile = (req, res) => {
 	const file = cache[req.params.image]?.file
 	if (!file) return (res.writeHead(400), res.end())
-	delete cache[req.params.image]	
 	res.writeHead(200,{'content-type':'image/png'})
 	return res.end(file,'binary')
 }
-
+exports.getHash  = (data) => 'file'+crypto.createHash('sha256').update(data).digest("hex")
+exports.getFile  = (key)  => {
+	count[key] = count[key] ? count[key]+1 : 1
+	if (count[key] < 6) delete cache[key]
+	return cache[key]
+}
+exports.saveFile = (key, data) => cache[key] = {date:Date.now(), file:data}
