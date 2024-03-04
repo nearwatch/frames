ad7ea51469db42a1872448720fa3cd1021a817e2
Near.Watch <89860717+nearwatch@users.noreply.github.com>
2024-03-04 20:09:27 +0500

    Update balance.cjs

--- frames/balance.cjs
+++ frames/balance.cjs
@@ -11,7 +11,16 @@ async function getUserInfo(fid){
 		if (!res.ok) return res.status == 404 ? {error:'User not found'} : {error:res.status}
 		const data = await res.json()
 		if (!data.found) return {error:'User not found'}
-		return {user:data.address.fname, address:data.address.data.address}
+		let result = {name:data.address.fname, address:data.address.data.address}
+		try{
+			const res = await fetch('https://hub.farcaster.standardcrypto.vc:2281/v1/userDataByFid?fid='+fid+'&user_data_type=4', {timeout:5000})
+			const data = await res.json()
+			result.image   = data?.messages.find(e => e.data?.userDataBody?.type == 'USER_DATA_TYPE_PFP')
+			result.display = data?.messages.find(e => e.data?.userDataBody?.type == 'USER_DATA_TYPE_DISPLAY')
+			if (result.display) result.display 	= result.display.data.userDataBody.value
+			if (result.image) 	result.image 	= result.image.data.userDataBody.value
+		}catch(error){console.log(error)}
+		return result
 	}catch(error){return {error}}
 }
 async function getUserByName(name){
@@ -48,38 +57,27 @@ async function getUserBalance(addr){
 	}catch(error){return {error}}
 }
 exports.getParams = async (query={}, payload) => {
-	const window = (text='', query={}) => {
+	const window = async (text='', query={}) => {
 		if (typeof text != 'string'){
 			query = text
 			text = ''
 		}
-		const values = {
-			name:'@'+query.name, 
-			fid:'fid: '+query.fid, 
-			address:query.address?query.address.substr(0,6)+'...'+query.address.substr(-4):'', 
-			updated:query.updated?query.updated:'', 
-			balance:query.balance?'$'+query.balance:''
-		}
-		const result = {
-			input: 'Enter fid, username or wallet', 
-			post_url: '',
-			no_cache: true,
-			buttons: [{label:'Get '+(query.address && !query.balance?'balance':'info'), action:'post'}]
+		const result = {input:'Enter fid, username or wallet', post_url:'', age:0, no_cache:true, buttons:[{label:'Get '+(query.address && !query.balance?'balance':'info'), action:'post'}]}
+		if (query.fid || query.address){
+			result.post_url = '?'+(query.fid?'fid='+query.fid+'&':'')+(query.address?'address='+query.address:'')
+			if (!query.balance && text.length == 0) delete result.input
 		}
 		let temp = template
-		for (const key of Object.keys(query)){
-			if (key!='details' && key!='balance' && key!='updated') result.post_url += (result.post_url.length?'&':'?')+key+'='+query[key]
-			if (values[key] && key!='updated' && key!='balance'){
-				temp = temp.replace('%text%',values[key])
-				if (!query.balance && text.length == 0) delete result.input
-			}
-		}
+		if (query.fid) 		temp = temp.replace('%text%','fid: '+query.fid)
+		if (query.name) 	temp = temp.replace('%text%','@'+query.name)
+		if (query.display) 	temp = temp.replace('%text%',query.display)
+		if (query.address) 	temp = temp.replace('%text%',query.address?query.address.substr(0,6)+'...'+query.address.substr(-4):'')
 		if (query.balance || text.length>0){ 
 			if (query.balance || text=='Data is being collected'){
 				if (query.balance){
 					const bt = query.details?.positions_distribution_by_type
 					const bc = query.details?.positions_distribution_by_chain
-					result.page_html = '<div>'+(query.name?'<h3>@'+query.name+'</h3>':'')+(query.fid?'fid: '+query.fid+'<br />':'')+(query.address?'address: '+query.address+'<br />':'')+(query.balance?'<h4>Total: $'+query.balance+'</h4>':'')+'</div>'
+					result.page_html = '<div>'+(query.name?'<h3>@'+query.name+(query.display?' ('+query.display+')':'')+'</h3>':'')+(query.fid?'fid: '+query.fid+'<br />':'')+(query.address?'address: '+query.address+'<br />':'')+(query.balance?'<h4>Total: $'+query.balance+'</h4>':'')+'</div>'
 					result.page_html += bt ? '<div>by type:<ul>'+Object.keys(bt).map(key => '<li>'+key+': $'+bt[key].toFixed(2)).join('</li>')+'</ul></div>' : ''
 					result.page_html += bc ? '<div>by chain:<ul>'+Object.keys(bc).map(key => '<li>'+key+': $'+bc[key].toFixed(2)).join('</li>')+'</ul></div>' : ''
 					result.page_html += query.updated ? '<div style="margin-top:50px; color:silver">updated '+query.updated+'</div>' : ''
@@ -89,7 +87,10 @@ exports.getParams = async (query={}, payload) => {
 			}
 			result.post_url = ''
 		}
-		temp = temp.replace('%text%',text).replace(/%text%/g,'').replace('%balance%',values.balance).replace('%updated%',values.updated)
+		temp =  temp.replace('%text%',text).replace(/%text%/g,'')
+					.replace('%balance%',query.balance?'$'+query.balance:'')
+					.replace('%updated%',query.updated?query.updated:'')
+		if (query.image) result.page_html = '<img src="'+query.image+'" width="120" height="120" style="float:left; margin:0px 10px 10px 0px;">'+result.page_html
 		result.svg = spyman.replace(/%message%/g, temp)
 		return result
 	}
@@ -102,6 +103,7 @@ exports.getParams = async (query={}, payload) => {
 		else return window('Wrong username')
 	}
 	if (query.address){
+		if (query.fid && user_data['fid'+query.fid]) query = {...query, ...user_data['fid'+query.fid]}
 		const balance = await getUserBalance(query.address)	
 		if (balance.error) return window('Error getting balances')
 		query.details = balance
@@ -116,8 +118,8 @@ exports.getParams = async (query={}, payload) => {
 		}
 		const userData = await getUserInfo(query.fid)		
 		if (userData.error || !userData.address) return window('wallet is not recognized',query)
-		query.name = userData.user
-		query.address = userData.address
+		if (userData.display) user_data['fid'+query.fid] = {name:userData.name, image:userData.image, display:userData.display}
+		query = {...query, ...userData}	
 		query.updated = new Date().toISOString().substr(0,16).replace('T',' ')
 		return window(query)
 	} 
